547ce0e1e30ca12b833265d1fd431df70cb12a5d
dbusato <davidedbusato@gmail.com>
2020-02-29 15:05:16 +0100

    Minor settings change

--- src/main/java/com/busatod/graphics/application/Settings.java
+++ src/main/java/com/busatod/graphics/application/Settings.java
@@ -6,13 +6,13 @@ public class Settings {
 	static final int HEIGHT = 768;
 	static final int NUM_BUFFERS = 3;
 	static final int BIT_DEPTH = 32;
-	static final int TARGET_FPS = 80;
+	static final int TARGET_FPS = 100;
 
 	public String title = TITLE;
 	public int width = WIDTH;
 	public int height = HEIGHT;
 	public int bitDepth = BIT_DEPTH;
-	public boolean fullScreen = true;
+	public boolean fullScreen = false;
 	public boolean debugInfo = true;
 	public int numBuffers = NUM_BUFFERS;
 	public int targetFps = TARGET_FPS;
