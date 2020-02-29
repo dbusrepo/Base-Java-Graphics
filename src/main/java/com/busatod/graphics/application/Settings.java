2b9153aab4239c3a34deba19845cf918c1868390
dbusato <davidedbusato@gmail.com>
2020-02-29 16:51:52 +0100

    Added c and r files

--- src/main/java/com/busatod/graphics/application/Settings.java
+++ src/main/java/com/busatod/graphics/application/Settings.java
@@ -2,7 +2,7 @@ package com.busatod.graphics.application;
 
 public class Settings {
 	static final String TITLE = "Java Graphics";
-	static final int WIDTH = 1366;
+	static final int WIDTH = 1024;
 	static final int HEIGHT = 768;
 	static final int NUM_BUFFERS = 3;
 	static final int BIT_DEPTH = 32;
@@ -12,7 +12,7 @@ public class Settings {
 	public int width = WIDTH;
 	public int height = HEIGHT;
 	public int bitDepth = BIT_DEPTH;
-	public boolean fullScreen = true;
+	public boolean fullScreen = false;
 	public boolean debugInfo = true;
 	public int numBuffers = NUM_BUFFERS;
 	public int targetFps = TARGET_FPS;
